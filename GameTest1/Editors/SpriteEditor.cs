d60b9247a0da5d44c616cc23fba256f7987ab1cf
xdanieldzd <xdanieldzd@googlemail.com>
2025-08-06 23:24:12 +0200

    Finish preliminary sprite frame editor work, start sprite animation editor

--- GameTest1/Editors/SpriteEditor.cs
+++ GameTest1/Editors/SpriteEditor.cs
@@ -2,6 +2,7 @@
 using GameTest1.Game.Sprites;
 using GameTest1.Utilities;
 using ImGuiNET;
+using System.Numerics;
 using System.Text.Json;
 
 namespace GameTest1.Editors
@@ -10,13 +11,19 @@ namespace GameTest1.Editors
     {
         public override string Name => "Sprite Editor";
 
-        const float zoom = 5f;
+        const float zoom = 4f;
 
         private Sprite? sprite;
 
         private string currentSpritePath = string.Empty;
+
+        private string frameEditorName = "Frame Editor";
         private bool isFrameEditorOpen = false, isFrameEditorFocused = false;
-        private int selectedFrame = -1;
+        private int selectedFrame = 0;
+
+        private string animEditorName = "Animation Editor";
+        private bool isAnimEditorOpen = false, isAnimEditorFocused = false;
+        private int selectedAnim = 0;
         //
 
         public override void Setup()
@@ -24,6 +31,7 @@ namespace GameTest1.Editors
             // TEST TEST TEST
             isOpen = true;
             isFrameEditorOpen = true;
+            isAnimEditorOpen = true;
             currentSpritePath = @"D:\Programming\UFO\Sprites\PlayerTest.json";
             sprite = JsonSerializer.Deserialize<Sprite>(File.ReadAllText(currentSpritePath), Assets.SerializerOptions);
         }
@@ -34,7 +42,7 @@ namespace GameTest1.Editors
 
             if (ImGui.Begin(Name, ref isOpen, ImGuiWindowFlags.AlwaysAutoResize))
             {
-                isFocused = ImGui.IsWindowFocused(ImGuiFocusedFlags.RootAndChildWindows) || isFrameEditorFocused;
+                isFocused = ImGui.IsWindowFocused(ImGuiFocusedFlags.RootAndChildWindows) || isFrameEditorFocused || isAnimEditorFocused;
 
                 ImGui.BeginGroup();
                 if (ImGui.Button("New Sprite"))
@@ -120,12 +128,14 @@ namespace GameTest1.Editors
                     ImGui.SliderFloat2("Origin", ref sprite.Origin, 0f, 1f);
                     ImGui.EndGroup();
 
-                    //
+                    ImGui.Separator();
 
-                    if (ImGui.Button("Frame Editor"))
-                        isFrameEditorOpen = true;
+                    if (ImGui.Button(frameEditorName)) { isFrameEditorOpen = true; ImGui.SetWindowFocus(frameEditorName); }
+                    ImGui.SameLine();
+                    if (ImGui.Button(animEditorName)) { isAnimEditorOpen = true; ImGui.SetWindowFocus(animEditorName); }
 
                     RunFrameEditor();
+                    RunAnimEditor();
                 }
             }
             isCollapsed = ImGui.IsWindowCollapsed();
@@ -136,89 +146,98 @@ namespace GameTest1.Editors
         {
             if (!isFrameEditorOpen || sprite == null) return;
 
-            ImGui.ShowDemoWindow();
-
-            if (ImGui.Begin("Frame Editor", ref isFrameEditorOpen, ImGuiWindowFlags.AlwaysAutoResize))
+            if (ImGui.Begin(frameEditorName, ref isFrameEditorOpen, ImGuiWindowFlags.AlwaysAutoResize))
             {
                 isFrameEditorFocused = ImGui.IsWindowFocused(ImGuiFocusedFlags.RootAndChildWindows);
 
-                if (ImGui.BeginTable("framestable", 6, ImGuiTableFlags.Borders | ImGuiTableFlags.SizingFixedFit | ImGuiTableFlags.NoHostExtendY, new(0f, 100f)))
+                ImGui.BeginGroup();
+                var currentFrameCount = sprite.Frames.Count;
+                if (currentFrameCount == 0) ImGui.BeginDisabled();
+                ImGui.SliderInt("Selected frame", ref selectedFrame, 0, sprite.Frames.Count == 0 ? 0 : sprite.Frames.Count - 1);
+                if (currentFrameCount == 0) ImGui.EndDisabled();
+                ImGui.SameLine();
+                ImGui.Dummy(new(10f, 0f));
+                ImGui.SameLine();
+                if (ImGui.Button("Add new frame")) sprite.Frames.Add(new());
+                if (currentFrameCount <= 0) ImGui.BeginDisabled();
+                ImGui.SameLine();
+                if (ImGui.Button("Remove selected frame"))
                 {
-                    ImGui.TableSetupColumn("Frame #");
-                    ImGui.TableSetupColumn("Source X");
-                    ImGui.TableSetupColumn("Source Y");
-                    ImGui.TableSetupColumn("Source Width");
-                    ImGui.TableSetupColumn("Source Height");
-                    ImGui.TableSetupColumn("Duration");
+                    sprite.Frames.RemoveAt(selectedFrame);
+                    selectedFrame = Math.Clamp(selectedFrame - 1, 0, sprite.Frames.Count);
+                }
+                if (currentFrameCount <= 0) ImGui.EndDisabled();
+                ImGui.EndGroup();
 
-                    ImGui.TableHeadersRow();
+                if (sprite.Frames.Count != 0)
+                {
+                    var maxX = sprite.SpritesheetTexture?.Width ?? 0;
+                    var maxY = sprite.SpritesheetTexture?.Height ?? 0;
 
-                    if (sprite.Frames.Count != 0)
+                    var frame = sprite.Frames[selectedFrame];
+
+                    ImGui.Separator();
+
+                    ImGui.BeginGroup();
+                    ImGui.SliderInt("Source X", ref frame.SourceCoord.X, 0, maxX, null, ImGuiSliderFlags.AlwaysClamp);
+                    ImGui.SliderInt("Source Y", ref frame.SourceCoord.Y, 0, maxY, null, ImGuiSliderFlags.AlwaysClamp);
+                    ImGui.SliderInt("Source Width", ref frame.SourceSize.X, 0, maxX, null, ImGuiSliderFlags.AlwaysClamp);
+                    ImGui.SliderInt("Source Height", ref frame.SourceSize.Y, 0, maxY, null, ImGuiSliderFlags.AlwaysClamp);
+                    ImGui.SliderFloat("Duration", ref frame.Duration, 0.1f, 15f, null, ImGuiSliderFlags.AlwaysClamp);
+                    ImGui.EndGroup();
+
+                    ImGui.SameLine();
+
+                    if (sprite.SpritesheetTexture != null)
                     {
-                        unsafe
+                        ImGui.BeginGroup();
+                        if (ImGui.BeginChild("spritesheet", Vector2.Zero, ImGuiChildFlags.Borders | ImGuiChildFlags.AlwaysAutoResize | ImGuiChildFlags.AutoResizeX | ImGuiChildFlags.AutoResizeY))
                         {
-                            var maxX = (float)(sprite.SpritesheetTexture?.Width ?? 0f);
-                            var maxY = (float)(sprite.SpritesheetTexture?.Height ?? 0f);
-
-                            var clipper = new ImGuiListClipperPtr(ImGuiNative.ImGuiListClipper_ImGuiListClipper());
-                            clipper.Begin(sprite.Frames.Count);
-                            while (clipper.Step())
+                            if (manager.ImGuiRenderer.BeginBatch(new(sprite.SpritesheetTexture.Width * zoom, sprite.SpritesheetTexture.Height * zoom), out var batcher, out var bounds))
                             {
-                                for (var i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
-                                {
-                                    var frame = sprite.Frames[i];
+                                batcher.CheckeredPattern(bounds, 8f, 8f, Color.Gray, Color.LightGray);
+                                batcher.Image(sprite.SpritesheetTexture, Vector2.Zero, Vector2.Zero, Vector2.One * zoom, 0f, Color.White);
 
-                                    ImGui.TableNextRow();
-
-                                    ImGui.PushID(i);
-
-                                    ImGui.TableSetColumnIndex(0);
-                                    ImGui.BeginDisabled();
-                                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-                                    var dummy = $"{i}";
-                                    ImGui.InputText($"##frame-id-{i}", ref dummy, 32, ImGuiInputTextFlags.ReadOnly);
-                                    ImGui.EndDisabled();
-
-                                    ImGui.TableSetColumnIndex(1);
-                                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-                                    if (ImGui.DragFloat($"##frame-x-{i}", ref frame.Rectangle.X, 0.25f, 0f, maxX)) frame.Rectangle.X = Math.Clamp(frame.Rectangle.X, 0f, maxX);
-
-                                    ImGui.TableSetColumnIndex(2);
-                                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-                                    if (ImGui.DragFloat($"##frame-y-{i}", ref frame.Rectangle.Y, 0.25f, 0f, maxY)) frame.Rectangle.Y = Math.Clamp(frame.Rectangle.Y, 0f, maxY);
-
-                                    ImGui.TableSetColumnIndex(3);
-                                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-                                    if (ImGui.DragFloat($"##frame-width-{i}", ref frame.Rectangle.Width, 0.25f, 0f, maxX)) frame.Rectangle.Width = Math.Clamp(frame.Rectangle.Width, 0f, maxX);
-
-                                    ImGui.TableSetColumnIndex(4);
-                                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-                                    if (ImGui.DragFloat($"##frame-height-{i}", ref frame.Rectangle.Height, 0.25f, 0f, maxY)) frame.Rectangle.Height = Math.Clamp(frame.Rectangle.Height, 0f, maxY);
-
-                                    ImGui.TableSetColumnIndex(5);
-                                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-                                    if (ImGui.DragFloat($"##frame-duration-{i}", ref frame.Duration, 0.25f, 0f, 100f)) frame.Duration = Math.Clamp(frame.Duration, 0f, 100f);
-                                    /*
-                                    if (ImGui.IsAnyItemHovered())
-                                    {
-                                        if (ImGui.BeginPopupContextItem())
-                                        {
-                                            selectedFrame = i;
-                                            ImGui.Text($"Popup for frame {selectedFrame}!");
-                                            if (ImGui.Button("Close"))
-                                                ImGui.CloseCurrentPopup();
-                                            ImGui.EndPopup();
-                                        }
-                                    }*/
-
-                                    ImGui.PopID();
-                                }
+                                batcher.RectLine(frame.SourceRectangle * zoom, 2f, Color.Red);
                             }
-                            clipper.Destroy();
+                            manager.ImGuiRenderer.EndBatch();
                         }
+                        ImGui.EndChild();
+                        ImGui.EndGroup();
                     }
                 }
-                ImGui.EndTable();
+            }
+            ImGui.End();
+        }
+
+        private void RunAnimEditor()
+        {
+            if (!isAnimEditorOpen || sprite == null) return;
+
+            if (ImGui.Begin(animEditorName, ref isAnimEditorOpen, ImGuiWindowFlags.AlwaysAutoResize))
+            {
+                isAnimEditorFocused = ImGui.IsWindowFocused(ImGuiFocusedFlags.RootAndChildWindows);
+
+                ImGui.BeginGroup();
+                var currentAnimCount = sprite.Animations.Count;
+                if (currentAnimCount == 0) ImGui.BeginDisabled();
+                ImGui.SliderInt("Selected animation", ref selectedAnim, 0, sprite.Animations.Count == 0 ? 0 : sprite.Animations.Count - 1);
+                if (currentAnimCount == 0) ImGui.EndDisabled();
+                ImGui.SameLine();
+                ImGui.Dummy(new(10f, 0f));
+                ImGui.SameLine();
+                if (ImGui.Button("Add new anim")) sprite.Animations.Add(new());
+                if (currentAnimCount <= 0) ImGui.BeginDisabled();
+                ImGui.SameLine();
+                if (ImGui.Button("Remove selected anim"))
+                {
+                    sprite.Animations.RemoveAt(selectedAnim);
+                    selectedAnim = Math.Clamp(selectedAnim - 1, 0, sprite.Animations.Count);
+                }
+                if (currentAnimCount <= 0) ImGui.EndDisabled();
+                ImGui.EndGroup();
+
+                //
             }
             ImGui.End();
         }
--- GameTest1/Game/Sprites/Frame.cs
+++ GameTest1/Game/Sprites/Frame.cs
@@ -6,10 +6,15 @@ namespace GameTest1.Game.Sprites
     public record Frame
     {
         [JsonInclude]
-        public Rect Rectangle = new();
+        public Point2 SourceCoord = Point2.Zero;
+        [JsonInclude]
+        public Point2 SourceSize = Point2.Zero;
         [JsonInclude]
         public float Duration = 0f;
 
         public Subtexture? Texture;
+
+        [JsonIgnore]
+        public Rect SourceRectangle => new(SourceCoord, SourceSize);
     }
 }
